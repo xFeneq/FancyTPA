d803ef76fbadd2bf2d8fd3633ad270ccc1652efb
xFeneq <filip.gub.1601@gmail.com>
2026-03-18 16:41:05 +0100

    test

---
+++ src/main/java/org/xFeneq/ftpa/FancyTPA.java
@@ -0,0 +1,17 @@
+package org.xFeneq.ftpa;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class FancyTPA extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
